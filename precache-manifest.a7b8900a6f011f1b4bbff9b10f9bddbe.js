1cf183a87d670cdbbbdcf67e34b85c6e2f3eacb4
Przemysław Matkowski <matkowski1994@gmail.com>
2019-12-06 14:29:00 +0100

    Updates

---
+++ precache-manifest.a7b8900a6f011f1b4bbff9b10f9bddbe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9de525d097d4ec3505ef89cae98e38c",
+    "url": "/lukas_spa/index.html"
+  },
+  {
+    "revision": "031a5ae9135dba2c0a6c",
+    "url": "/lukas_spa/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b6a30219641d166f0e33",
+    "url": "/lukas_spa/static/css/main.23470ae8.chunk.css"
+  },
+  {
+    "revision": "031a5ae9135dba2c0a6c",
+    "url": "/lukas_spa/static/js/2.b78e225a.chunk.js"
+  },
+  {
+    "revision": "b6a30219641d166f0e33",
+    "url": "/lukas_spa/static/js/main.46e7ca81.chunk.js"
+  },
+  {
+    "revision": "8d3164eb9157aa25deca",
+    "url": "/lukas_spa/static/js/runtime-main.b6ef687a.js"
+  },
+  {
+    "revision": "500f89a4af777d2e9a320b82885c8f40",
+    "url": "/lukas_spa/static/media/home.500f89a4.jpg"
+  },
+  {
+    "revision": "5ca74619e259bad4ff67348b38a5efe4",
+    "url": "/lukas_spa/static/media/koperta.5ca74619.jpg"
+  },
+  {
+    "revision": "a82f29299ab80cccc9a6864b5ff67933",
+    "url": "/lukas_spa/static/media/man.a82f2929.jpg"
+  },
+  {
+    "revision": "2389c807c484b5787d20cb3a47e0865d",
+    "url": "/lukas_spa/static/media/merc_b.2389c807.jpg"
+  },
+  {
+    "revision": "13009190c3fc282852542f1633165796",
+    "url": "/lukas_spa/static/media/merc_w.13009190.jpg"
+  },
+  {
+    "revision": "641a1dad4219bc66bfa69c4e4a38088b",
+    "url": "/lukas_spa/static/media/merc_y.641a1dad.jpg"
+  }
+]);
